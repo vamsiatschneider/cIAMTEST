6e24b0d4b51a3ac5969b815c2cbfe32929afd182
Suresh Bachu <Suresh.Bachu@non.schneider-electric.com>
2018-08-08 16:26:07 +0530

    DirectAPI error codes updated

---
+++ idmsc_restapi/schneider-idms-service/src/main/java/com/schneider/idms/common/ErrorCodeConstants.java
@@ -0,0 +1,21 @@
+package com.schneider.idms.common;
+
+public class ErrorCodeConstants {
+
+	public static final String BAD_REQUEST = "BAD_REQUEST";
+	
+	public static final String BADREQUEST_MESSAGE = "||description of the error from backend||";
+	
+	public static final String UNAUTHORIZED_REQUEST = "INVALID_SESSION_ID";
+	
+	public static final String UNAUTHORIZED_MESSAGE = "Session expired or invalid";
+	
+	public static final String CONFLICT_REQUEST = "CREATE_FAILED ";
+	
+	public static final String CONFLICT_MESSAGE = "||description of the error from backend ||";
+	
+	public static final String SERVER_ERROR_REQUEST = "INTERNAL_SERVER_ERROR ";
+	
+	public static final String SERVER_ERROR_MESSAGE = "||description of the error from backend||";
+	
+}
