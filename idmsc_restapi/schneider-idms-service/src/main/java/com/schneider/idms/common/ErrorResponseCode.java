ced03798471433bc535baac4a50fd9ad115674cf
Santosh kumar <Srihari.Pamidipati@non.schneider-electric.com>
2018-08-18 11:44:35 +0530

    Error code status modification

--- idmsc_restapi/schneider-idms-service/src/main/java/com/schneider/idms/common/ErrorResponseCode.java
+++ idmsc_restapi/schneider-idms-service/src/main/java/com/schneider/idms/common/ErrorResponseCode.java
@@ -3,14 +3,19 @@
  */
 package com.schneider.idms.common;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
 /**
  * @author SESA508936
  *
  */
 public class ErrorResponseCode {
 
+	@JsonInclude(Include.NON_NULL)
 	private String code;
 	
+	@JsonInclude(Include.NON_NULL)
 	private String message;
 	
 	public String getCode() {
--- idmsc_restapi/schneider-idms-service/src/main/java/com/schneider/idms/common/ResponseCodeStatus.java
+++ idmsc_restapi/schneider-idms-service/src/main/java/com/schneider/idms/common/ResponseCodeStatus.java
@@ -3,11 +3,16 @@
  */
 package com.schneider.idms.common;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
 /**
  * @author SESA508936
  *
  */
 public class ResponseCodeStatus extends ErrorResponseCode {
+	
+	@JsonInclude(Include.NON_NULL)
 	private String status;
 
 	public String getStatus() {
