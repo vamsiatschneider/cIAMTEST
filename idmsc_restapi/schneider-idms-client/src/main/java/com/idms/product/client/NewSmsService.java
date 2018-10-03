c657afdc92be3a9c05782784e5fbc33ed7a1abe0
Santosh kumar <Srihari.Pamidipati@non.schneider-electric.com>
2018-10-03 19:51:48 +0530

    smsgateway

--- idmsc_restapi/schneider-idms-client/src/main/java/com/idms/product/client/NewSmsService.java
+++ idmsc_restapi/schneider-idms-client/src/main/java/com/idms/product/client/NewSmsService.java
@@ -1,7 +1,6 @@
 package com.idms.product.client;
 
 import javax.ws.rs.GET;
-import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
@@ -10,7 +9,6 @@ import javax.ws.rs.core.Response;
 public interface NewSmsService {
 
 	@GET
-	@Path("/mdsmssend")
 	Response sendSMSCode(@QueryParam("sn") String sn, @QueryParam("pwd") String pwd,
 			@QueryParam("mobile") String mobile, @QueryParam("content") String content);
 
