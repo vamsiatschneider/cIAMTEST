3ba33067bd788d86ec2c1cbf45161408ac6e522f
shyam-sivaraman-se <Shyam.Sivaraman@non.schneider-electric.com>
2018-12-07 19:17:59 +0530

    Merge 17: Complete removal of UIMS-WS library code

--- idmsc_restapi/schneider-idms-service/src/main/java/com/se/uims/usermanager/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://uimsv22.service.ims.schneider.com/")
-package com.se.uims.usermanager;
--- idmsc_restapi/schneider-idms-service/src/main/java/com/uims/companymanager/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://uimsv2.service.ims.schneider.com/")
-package com.uims.companymanager;
