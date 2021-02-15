446428863dedf096b59778147c16cbce913009ad
sesa565217 <jasmine.pahwa@se.com>
2021-02-16 00:26:10 +0530

    CIAM-7046

--- idmsc_restapi/schneider-idms-service/src/test/java/com/idms/service/DtoMockData.java
+++ idmsc_restapi/schneider-idms-service/src/test/java/com/idms/service/DtoMockData.java
@@ -14,7 +14,6 @@ import com.idms.model.SendInvitationRequest;
 import com.idms.model.UpdatePasswordRequest;
 import com.idms.model.UpdateUserRequest;
 import com.idms.model.UserAILRecord;
-import com.idms.model.UserMFADataRequest;
 import com.idms.product.model.OpenAmUser;
 import com.idms.product.model.OpenAmUserInput;
 import com.idms.product.model.OpenAmUserRequest;
